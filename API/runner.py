7f7f36f1ed184b37c2788c5428888b16a08df272
Bhushan Amol Anokar <75465521+bhushananokar@users.noreply.github.com>
2025-01-23 19:18:59 +0530

    Delete API directory

--- API/runner.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://localhost:8000/predict"
-symptoms = "Frequent urination,Increased thirst,Unexplained weight loss,Fatigue , Blurred vision ,Slow-healing sores"
-response = requests.post(url, json={"symptoms": symptoms})
-print(response.text)
